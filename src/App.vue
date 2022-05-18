5abaa6b8f2b72f12ecebafd6f10e0152ca5f952b
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-05-18 17:10:56 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <table class="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Cards</th>
+            <th>Elixir</th>
+            <th>Type</th>
+            <th>Rarity</th>
+            <th>Arena</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="card in cards" :key="card.id">
+            <td>
+              {{ card.name }}
+            </td>
+            <td>
+              {{ card.elixir }}
+            </td>
+            <td>
+              {{ card.type }}
+            </td>
+            <td>
+              {{ card.rarity }}
+            </td>
+            <td>
+              {{ card.arena }}
+            </td>
+            <td>
+              {{ card.description }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      cards: [],
+    };
+  },
+  async mounted() {
+    const res = await fetch(
+      "https://royaleapi.github.io/cr-api-data/json/cards.json"
+    );
+    const data = await res.json();
+    console.log(data);
+    this.cards = data;
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
