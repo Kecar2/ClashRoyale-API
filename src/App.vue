0754c165c637a855594e49ad53564b093c6f3662
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-05-18 18:11:42 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="container">
     <div class="row">
+      <h1>Clash Royale Cards</h1>
+
+      <input
+        type="text"
+        class="form-control bg-dark text-light rounded-0 border-0 my-4"
+        placeholder="Search Card"
+        @keyup="searchCard()"
+        v-model="textSearch"
+      />
+
       <table class="table table-dark table-hover">
         <thead>
           <tr>
-            <th>Cards</th>
-            <th>Elixir</th>
-            <th>Type</th>
-            <th>Rarity</th>
-            <th>Arena</th>
-            <th>Description</th>
+            <th v-for="title in titles" :key="title">
+              {{ title }}
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="card in cards" :key="card.id">
+          <tr v-for="(card, index) in filteredCards" :key="card.id">
+            <td class="text-muted">
+              {{ index + 1 }}
+            </td>
             <td>
               {{ card.name }}
             </td>
             <td>
               {{ card.elixir }}
-            </td>
-            <td>
-              {{ card.type }}
             </td>
             <td>
               {{ card.rarity }}
@@ -45,15 +52,26 @@ export default {
   data() {
     return {
       cards: [],
+      filteredCards: [],
+      titles: ["#", "Name", "Elixir", "Rarity", "Arena", "Description"],
+      textSearch: ''
     };
   },
   async mounted() {
     const res = await fetch(
-      "https://royaleapi.github.io/cr-api-data/json/cards.json"
+      "https://royaleapi.github.io/cr-api-data/json/cards_i18n.json"
     );
     const data = await res.json();
     console.log(data);
     this.cards = data;
+    this.filteredCards = data
+  },
+  methods: {
+
+    searchCard() {
+    this.filteredCards = this.cards.filter(card => card.name.toLowerCase().includes(this.textSearch.toLowerCase()))
+    },
+
   },
 };
 </script>
